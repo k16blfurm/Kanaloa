aa130fb303d6274aeb98bf117f2cfa4b9b8995bc
dtru8 <40726201+dtru8@users.noreply.github.com>
2018-09-08 13:47:20 -1000

    Add files via upload
    Alter acceleration limits code to account for individual conditions of motor controllers

--- WAMV/Arduino/acceleration_limits/acceleration_limits.ino
+++ WAMV/Arduino/acceleration_limits/acceleration_limits.ino
@@ -19,55 +19,58 @@ void accellimit(int Q1L,int Q2L,int Q3L,int Q4L,int Q1Out,int Q2Out,int Q3Out,in
   //the motor controllers to the desired voltage
   //Initiate I2c Communication with DACs
      dac.begin(0x62);
-     //Ports on Multiplexers
-     const byte Q1Port=0;
-     const byte Q2Port=1;
-     const byte Q3Port=3;
-     const byte Q4Port=2;
-  //Controlling Acceleration    
-      while(Q1L<Q1Out || Q2L< Q2Out || Q3L<Q3Out || Q4L<Q4Out){
-          //Output Voltage to Q1
-          bool isdecel=false;
-          tcaselect(Q1Port);
-          dac.setVoltage(Q1L, true); 
-            if (Q1L<Q1Out){Q1L=scaledaccel(Q1L,Q1Out,isdecel);} //If the old value is not the same as the new value, increment closer the new voltage
-          //Output Voltage to Q2
-          tcaselect(Q2Port);
-          dac.setVoltage(Q2L, true);
-            if (Q2L<Q2Out){Q2L=scaledaccel(Q2L,Q2Out,isdecel);}
-          //Output to Q3
-          tcaselect(Q3Port);
-          dac.setVoltage(Q3L, true);
-            if(Q3L<Q3Out){Q3L=scaledaccel(Q3L,Q3Out,isdecel);}
-          //Output to Q4
-          tcaselect(Q4Port);
-          dac.setVoltage(Q4Out, true);
-            if (Q4L<Q4Out){Q4L=scaledaccel(Q4L,Q4Out,isdecel);}
+  const bool forward=true;
+  const bool backward=false;
+  bool Q1Dir;
+  bool Q2Dir;
+  bool Q3Dir;
+  bool Q4Dir;
+  //Determine which motor is decelerating
+  Q1Dir = (Q1Out-Q1L < 0)?backward:forward; //ternary operators Basically a shorthand if, else statement;
+  Q2Dir = (Q2Out-Q2L < 0)?backward:forward;
+  Q3Dir = (Q3Out-Q3L < 0)?backward:forward;
+  Q4Dir = (Q4Out-Q4L < 0)?backward:forward;
+
+  //While the motors need to change (Q1L =! Q1) 
+   bool Q1needchange=true;
+   bool Q2needchange=true;
+   bool Q3needchange=true;
+   bool Q4needchange=true;    
+      while(Q1needchange || Q2needchange || Q3needchange || Q4needchange){
+          //If motor one still needs to change
+          if(Q1L != Q1Out){
+            tcaselect(Q1Port); //Open the port to the motor 1 DAC.
+            dac.setVoltage(Q1L, true); //Set the voltage to send to the motor
+            Q1L=scaledaccel(Q1L,Q1Out,Q1Dir); //Readjust change point        
+          }
+          else{Q1needchange=false;}
+          
+          //If motor two still needs to change
+           if(Q2L != Q2Out){
+            tcaselect(Q2Port); //Open the port to the motor 2 DAC.
+            dac.setVoltage(Q2L, true); //Set the voltage to send to the motor
+            Q1L=scaledaccel(Q2L,Q2Out,Q2Dir); //Readjust change point        
+          }
+          else{Q2needchange=false;}
+          Serial.println(Q1L);
+          //If motor three still needs to change
+          if(Q3L != Q3Out){
+            tcaselect(Q3Port); //Open the port to the motor 3 DAC.
+            dac.setVoltage(Q3L, true); //Set the voltage to send to the motor
+            Q1L=scaledaccel(Q3L,Q3Out,Q3Dir); //Readjust change point        
+          }
+          else{Q3needchange=false;}
+          //If motor four still needs to change
+          if(Q4L != Q4Out){
+            tcaselect(Q4Port); //Open the port to the motor 4 DAC.
+            dac.setVoltage(Q4L, true); //Set the voltage to send to the motor
+            Q1L=scaledaccel(Q4L,Q4Out,Q4Dir); //Readjust change point        
+          }
+          else{Q4needchange=false;}
       delay(100); //I actually was not sure how much delay I should give to the incrementations.
       }
-      //Controlling Deceleration 
-    while(Q1L>Q1Out || Q2L > Q2Out || Q3L >Q3Out || Q4L >Q4Out){
-          //Output to Q1
-          bool isdecel=true;
-          tcaselect(Q1Port);
-          dac.setVoltage(Q1L, true); 
-            if (Q1L>Q1Out){Q1L=scaledaccel(Q1L,Q1Out,isdecel);} //Slowly Speed up (increment by 1 bit)
-          //Output to Q2
-          tcaselect(Q2Port);
-          dac.setVoltage(Q2L, true);
-            if (Q2L>Q2Out){Q2L=scaledaccel(Q2L,Q2Out,isdecel);}
-          //Output to Q3
-          tcaselect(Q3Port);
-          dac.setVoltage(Q3L, true);
-            if(Q3L>Q3Out){Q3L=scaledaccel(Q3L,Q3Out,isdecel);}
-          //Output to Q4
-          tcaselect(Q4Port);
-          dac.setVoltage(Q4Out, true);
-            if (Q4L>Q4Out){Q4L=scaledaccel(Q4L,Q4Out,isdecel);}
-      delay(100);
-      }
-
     }
+
 
 int scaledaccel(int oval, int nval, bool isdecel){
 //Auxillary function to accellimit
