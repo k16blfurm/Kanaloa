7ddbe60e05b458efa1c3b804cadaaf46c49ea1c6
dtru8 <40726201+dtru8@users.noreply.github.com>
2018-09-13 21:21:07 -1000

    Add files via upload
    This is the "final" version of the master code I was working on. I apologize but I have limited access to internet right now. All changes should be made to this one (including whatever Kai just made, sorry.)

--- WAMV/Arduino/_2018.08.ind_mainMegaWIP/_2018.08.ind_mainMegaWIP.ino
+++ WAMV/Arduino/_2018.08.ind_mainMegaWIP/_2018.08.ind_mainMegaWIP.ino
@@ -2,13 +2,7 @@
  * Dev notes: With this program, what we want it to do is, instead of using PWM, we need to send an Analog signal to the motor controller.
  */
 
-//Change battery read function to read 36V (or maybe max 50V).
-//Add manualPWM variable
-//Make functions for each of the direction motor controller H-bridges
-
-
-
-
+//Change battery read function to read 36V (Right now it only reads up to 15V.
 
 
 // Library inclusions
@@ -42,7 +36,7 @@ const byte greenLightPin=38;    // digital out to green safety light relay (HIGH
 const byte thrusterKillPin=16;  // digital out to thruster kill switch
 const byte armReelKillPin=18;   // digital out to arm-reel kill switch
 /*The pin values for yellowLight-armReal are made up*/
-const byte hbridgepins[16]={16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31}; 
+const byte hbridgepins[16]={16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31}; //Change as needed
 //Every 4 values of array corresponds to 1 set of H-bridge connections to motor controllers (first 4 go to Q1, last 4 go to Q4), used for H-bridge functions
 //For a set of 4 H-bridge pins, the order of the values matter. Here is how each pin value corresponds to a set of 4 values. 
 //First  value of 4: pforward, green P+ 
@@ -70,8 +64,8 @@ int autoQ4;
 int invalid_input = 0;
 
 /*Motor range values*/
-const int fullreverse= -2948; //194 Was the old All but 1 blink. 192 all but 1 solid. Basically, if we want to be really precise, we would need to find each individual reverse and forward.
-const int fullforward=2948; //This value causes one of the motors to be solid 
+const int fullreverse= -2930; 
+const int fullforward=2930;
 const int neutral=0;
 
 /*-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
@@ -289,7 +283,7 @@ void motormap(int Q1, int Q2, int Q3, int Q4, int Q1Last, int Q2Last, int Q3Last
    }
    
     // Adjust motor drive output based on main battery voltage
-    const byte maxThrusterVolt=12;
+    const byte maxThrusterVolt=36;
     Q1Out = neutral+(Q1Out-neutral)* maxThrusterVolt/voltMain;
     Q2Out = neutral+(Q2Out-neutral)* maxThrusterVolt/voltMain;
     Q3Out = neutral+(Q3Out-neutral)* maxThrusterVolt/voltMain;
@@ -306,8 +300,7 @@ void motormap(int Q1, int Q2, int Q3, int Q4, int Q1Last, int Q2Last, int Q3Last
       }
       
     // Output to motor drivers
-     //accellimits(Q1Last,Q2Last,Q3Last,Q4Last,Q1Out,Q2Out,Q3Out,Q4Out);
-
+     accellimits(Q1Last,Q2Last,Q3Last,Q4Last,Q1Out,Q2Out,Q3Out,Q4Out);
     
     // Publish to ROS (For troubleshooting);
     Q1Msg.data = Q1Out;
@@ -333,9 +326,92 @@ void tcaselect(uint8_t i) {
 
 //Add acceleration limits code when finished from accellimits file
 
+void accellimits(int Q1L,int Q2L,int Q3L,int Q4L,int Q1Out,int Q2Out,int Q3Out,int Q4Out)
+{
+  //This program prevents sudden change in voltage to the motor controllers by slowly incrementing the voltage sent to 
+  //the motor controllers to the desired voltage
+  //Initiate I2c Communication with DACs
+  dac.begin(0x62);
+  const bool forward=true;
+  const bool backward=false;
+  const byte Q1Port=0;
+  const byte Q2Port=1;
+  const byte Q3Port=2;
+  const byte Q4Port=3;
+  bool Q1Dir;
+  bool Q2Dir;
+  bool Q3Dir;
+  bool Q4Dir;
+  //Determine which motor is decelerating
+  Q1Dir = (Q1Out-Q1L < 0)?backward:forward; //ternary operators Basically a shorthand if, else statement;
+  Q2Dir = (Q2Out-Q2L < 0)?backward:forward;
+  Q3Dir = (Q3Out-Q3L < 0)?backward:forward;
+  Q4Dir = (Q4Out-Q4L < 0)?backward:forward;
 
+  //While the motors need to change (Q1L =! Q1) 
+   bool Q1needchange=true;
+   bool Q2needchange=true;
+   bool Q3needchange=true;
+   bool Q4needchange=true;    
+      while(Q1needchange || Q2needchange || Q3needchange || Q4needchange){
+          //If motor one still needs to change
+          if(Q1L != Q1Out){
+            tcaselect(Q1Port); //Open the port to the motor 1 DAC.
+            dac.setVoltage(abs(Q1L), false); //Set the voltage to send to the motor
+            Q1L=scaledaccel(Q1L,Q1Out,Q1Dir); //Readjust change point
+            (Q1L<0)?qonereverse(hbridgepins,true):qonereverse(hbridgepins,false);        
+          }
+          else{Q1needchange=false;}
+          
+          //If motor two still needs to change
+           if(Q2L != Q2Out){
+            tcaselect(Q2Port); //Open the port to the motor 2 DAC.
+            dac.setVoltage(abs(Q2L), false); //Set the voltage to send to the motor
+            Q2L=scaledaccel(Q2L,Q2Out,Q2Dir); //Readjust change point
+            (Q2L<0)?qtworeverse(hbridgepins,true):qtworeverse(hbridgepins,false);         
+          }
+          else{Q2needchange=false;}
+          //If motor three still needs to change
+          if(Q3L != Q3Out){
+            tcaselect(Q3Port); //Open the port to the motor 3 DAC.
+            dac.setVoltage(abs(Q3L), false); //Set the voltage to send to the motor
+            Q3L=scaledaccel(Q3L,Q3Out,Q3Dir); //Readjust change point
+            (Q3L<0)?qthreereverse(hbridgepins,true):qthreereverse(hbridgepins,false);         
+          } 
+          else{Q3needchange=false;}
+          //If motor four still needs to change
+          if(Q4L != Q4Out){
+            tcaselect(Q4Port); //Open the port to the motor 4 DAC.
+            dac.setVoltage(abs(Q4L), false); //Set the voltage to send to the motor
+            Q4L=scaledaccel(Q4L,Q4Out,Q4Dir); //Readjust change point
+            (Q4L<0)?qfourreverse(hbridgepins,true):qfourreverse(hbridgepins,false);         
+          }
+          else{Q4needchange=false;}
+      delay(60); 
+      }
+    }
+
+int scaledaccel(int oval, int nval, bool isaccel){
+//Auxillary function to accellimit
+//Controls how quickly the DAC needs to change voltage depending on the difference in voltage represented using 12 bit resolution. 
+int diff=0;
+diff=abs(nval-oval);
+if(isaccel){//If the WAMV is decelerating
+    if(diff >= 1000){return oval+200;}
+    if(diff >= 100){return oval+50;}
+    if(diff >= 10){return oval+5;}
+    if(diff >= 1){return oval+1;}
+}
+else{ //If WAMV is accelerating
+    if(diff >= 1000){return oval-200;}
+    if(diff >= 100){return oval-50;}
+    if(diff >= 10){return oval-5;}
+    if(diff >= 1){return oval-1;}
+}
+}
 
 void qonereverse(const byte hpins[16], bool reverse){
+  //A function for controlling the H-bridge of motor controller 1 (Q1).
   if (reverse){ //If the motor does need to go in reverse
   digitalWrite(hpins[0],LOW); 
   digitalWrite(hpins[1],HIGH);
@@ -351,6 +427,7 @@ void qonereverse(const byte hpins[16], bool reverse){
 }
 
 void qtworeverse(const byte hpins[16], bool reverse){
+  //A function for controlling the H-bridge of motor controller 2 (Q2).
   if (reverse){ //If the motor does need to go in reverse
   digitalWrite(hpins[4],LOW); 
   digitalWrite(hpins[5],HIGH);
@@ -366,6 +443,7 @@ void qtworeverse(const byte hpins[16], bool reverse){
 }
 
 void qthreereverse(const byte hpins[16], bool reverse){
+  //A function for controlling the H-bridge of motor controller 3 (Q3).
   if (reverse){ //If the motor does need to go in reverse
   digitalWrite(hpins[8],LOW); 
   digitalWrite(hpins[9],HIGH);
@@ -381,6 +459,7 @@ void qthreereverse(const byte hpins[16], bool reverse){
 }
 
 void qfourreverse(const byte hpins[16], bool reverse){
+  //A function for controlling the H-bridge of motor controller 4 (Q4).
   if (reverse){ //If the motor does need to go in reverse
   digitalWrite(hpins[12],LOW); 
   digitalWrite(hpins[13],HIGH);
